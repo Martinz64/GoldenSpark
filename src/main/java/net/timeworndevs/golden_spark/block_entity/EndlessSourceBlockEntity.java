11b3ce0b38354b4d5199162211cdb05705b35f75
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2023-07-26 14:43:29 -0500

    Working on the energy source

--- src/main/java/net/timeworndevs/golden_spark/block_entity/EndlessSourceBlockEntity.java
+++ src/main/java/net/timeworndevs/golden_spark/block_entity/EndlessSourceBlockEntity.java
@@ -5,9 +5,25 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
 import net.timeworndevs.golden_spark.init.GSBlockEntityTypes;
+import team.reborn.energy.api.base.SimpleEnergyStorage;
 
 public class EndlessSourceBlockEntity extends BlockEntity {
     public EndlessSourceBlockEntity( BlockPos pos, BlockState state) {
         super(GSBlockEntityTypes.ENDLESS_SOURCE_TYPE, pos, state);
     }
+
+    public final SimpleEnergyStorage energyStorage = new SimpleEnergyStorage(100, 100, 100) {
+        @Override
+        protected void onFinalCommit() {
+            markDirty();
+        }
+    };
+
+    public void tick() {
+        if (!world.isClient && energyStorage.amount >=10) {
+            energyStorage.amount -= 10;
+            markDirty();
+        }
+    }
+    
 }
